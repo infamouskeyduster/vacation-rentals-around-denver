1905b1edec1484b3d1d0e4979cc3dd79b999e4f3
John Watterson <john@breakthrewbrew.com>
2020-05-12 16:35:13 -0600

    Set up index.js and App into component directory

---
+++ src/components/App/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import './App.css';
+import Login from '../Login/Login';
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Login />
+      </div>
+    );
+  }
+}
+
+export default App;
